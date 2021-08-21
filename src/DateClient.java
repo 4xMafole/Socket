55fae88ac7e2c922859a9ce8720037d586ae5fd9
4xMafole <4cruxt.unknown@gmail.com>
2021-08-21 21:28:54 +0300

    Added comments to the Trivial Server classes

--- src/DateClient.java
+++ src/DateClient.java
@@ -2,6 +2,11 @@ import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
 
+/**
+ * A command line client for the date server. Requires the IP address of the
+ * server as the sole argument. Exits after printing the response.
+ */
+
 public class DateClient 
 {
     public static void main(String[] args) throws IOException
--- src/DateServer.java
+++ src/DateServer.java
@@ -3,6 +3,13 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.util.Date;
 
+/**
+ * A simple TCP server. When a client connects, it sends the client the current
+ * datetime, then closes the connection. This is arguably the simplest server
+ * you can write. Beware though that a client has to be completely served its
+ * date before the server will be able to handle another client.
+ */
+
 public class DateServer 
 {
     public static void main(String[] args) throws IOException
