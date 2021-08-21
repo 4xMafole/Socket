05d78b8feadff65faebf0e82247a3d91b238a7a1
4xMafole <4cruxt.unknown@gmail.com>
2021-08-21 20:54:46 +0300

    first commit

---
+++ src/DateClient.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class DateClient 
+{
+    public static void main(String[] args) throws IOException
+    {
+        if(args.length != 1)
+        {
+            System.out.println("Pass the server IP as the sole command line");
+            return;
+        }
+
+        var socket = new Socket(args[0], 59090);
+        var in = new Scanner(socket.getInputStream());
+        System.out.println("Server response: " + in.nextLine());
+    }    
+}
---
+++ src/DateServer.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.util.Date;
+
+public class DateServer 
+{
+    public static void main(String[] args) throws IOException
+    {
+        try(var listener = new ServerSocket(59090))
+        {
+            System.out.println("Server date is running..");
+            while(true)
+            {
+                try(var socket = listener.accept())
+                {
+                    var out = new PrintWriter(socket.getOutputStream(), true);
+                    out.println(new Date().toString());
+                }
+            }
+        }
+    }
+}
